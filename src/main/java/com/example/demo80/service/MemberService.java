bad5fbaf16777876eeeb8bb6c72160e557090667
twl107 <ds1202kr@gmail.com>
2025-11-10 19:16:19 +0900

    20251110_1850 study

--- src/main/java/com/example/demo80/service/MemberService.java
+++ src/main/java/com/example/demo80/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.demo80.service;
 
+import com.example.demo80.entity.Member;
 import com.example.demo80.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,8 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
 
+    public Member saveMember(Member member) {
 
-
+        return memberRepository.save(member);
+    }
 }
